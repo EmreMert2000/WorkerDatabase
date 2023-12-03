db0ac06ccd9c88e8425d0acc9ba5cc5c083d8585
Emre Mert <61905534+EmreMert2000@users.noreply.github.com>
2023-12-03 12:53:35 +0300

    ViewBinding Functions

--- app/src/main/java/com/example/roomdatabaseintroduction/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabaseintroduction/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.roomdatabaseintroduction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomdatabaseintroduction.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.button.setOnClickListener{
+        }
     }
 }
